ee97623ef430ed58998f9d8eb1a832fe3fca677b
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-12-06 10:09:09 +0530

    Adding Route Handlers

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,62 @@
+import React,{Component} from 'react'
+import axios from 'axios'
+import Post from '../../../components/Post/Post'
+import './Posts.css'
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        let posts = axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            posts = response.data.slice(0,4)
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Dev'
+                }
+            })
+            this.setState({posts: updatedPosts})
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
+    render() {
+        let posts = <p style={{textAlign:'center'}}>Something Went Wrong</p>
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)}    
+                />
+            })
+        }
+
+        return (
+            <section className="Posts">
+            {posts}
+            </section>
+        )
+    }
+}
+
+export default Posts;
+
+
+
+// <section>
+// <FullPost id={this.state.selectedPostId} />
+// </section>
+// <section>
+// <NewPost />
+// </section>
